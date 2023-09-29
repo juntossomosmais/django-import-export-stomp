23193d17f89a64ef9fc76d1118254b0990566ced
MatheusLeall <matheuscardoso.pro@gmail.com>
2023-09-29 09:24:27 -0300

    feat(tests): add tests for fields module and fix test formats

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,9 +1,11 @@
 import contextlib
 
+from unittest.mock import Mock
+from unittest.mock import patch
+
 import import_export
 import pytest
 
-from django.template import exceptions
 from django.test import override_settings
 
 from import_export_stomp.models.exportjob import ExportJob
@@ -16,6 +18,13 @@ from import_export_stomp.utils import send_export_job_completion_mail
 
 
 @pytest.fixture
+def export_job_mail():
+    return Mock(
+        updated_by=Mock(email="test@example.com"),
+    )
+
+
+@pytest.fixture
 def export_job():
     return ExportJob(
         app_label="import_export_stomp",
@@ -24,54 +33,104 @@ def export_job():
     )
 
 
-def test_get_formats():
-    CSV = import_export.formats.base_formats.CSV
-    XLSX = import_export.formats.base_formats.XLSX
-    with contextlib.suppress(ImportError):
-        formats = get_formats()
-        assert CSV in formats
-        assert XLSX in formats
+@pytest.fixture
+def mock_get_template():
+    def mock_template_render(template_name, context=None):
+        class MockTemplate:
+            def render(self, context):
+                return (
+                    f"HTML content for {template_name}",
+                    f"Text content for {template_name}",
+                )
+
+        return MockTemplate()
+
+    return mock_template_render
 
 
-@override_settings(EXPORT_JOB_COMPLETION_MAIL_TEMPLATE="test_template.html")
-def test_build_html_and_text_message():
-    with contextlib.suppress(ImportError, exceptions.TemplateDoesNotExist):
+@pytest.mark.django_db
+class TestUtils:
+    def test_get_formats(self):
+        CSV = import_export.formats.base_formats.CSV
+        XLSX = import_export.formats.base_formats.XLSX
+        with contextlib.suppress(ImportError):
+            formats = get_formats()
+            assert CSV in formats
+            assert XLSX in formats
+
+    @override_settings(EXPORT_JOB_COMPLETION_MAIL_TEMPLATE="test_template.html")
+    @patch("import_export_stomp.utils.get_template")
+    def test_build_html_and_text_message(self, mock_get_template):
         template_name = "test_template.html"
         context = {"key": "value"}
-        html_message, text_message = build_html_and_text_message(template_name, context)
-        assert "key" in html_message
-        assert "value" in html_message
-        assert "key" in text_message
-        assert "value" in text_message
 
+        _, _ = build_html_and_text_message(template_name, context)
 
-def test_get_export_job_mail_context(export_job):
-    with contextlib.suppress(ImportError):
-        context = get_export_job_mail_context(export_job)
-        assert "app_label" in context
-        assert "model" in context
-        assert "link" in context
-        assert context["app_label"] == "import_export_stomp"
-        assert context["model"] == "MyModel"
-        assert "http://example.com" in context["link"]
+        mock_get_template.assert_called_once_with(template_name)
 
+    def test_get_export_job_mail_context(self, export_job):
+        with contextlib.suppress(ImportError):
+            context = get_export_job_mail_context(export_job)
+            assert "app_label" in context
+            assert "model" in context
+            assert "link" in context
+            assert context["app_label"] == "import_export_stomp"
+            assert context["model"] == "MyModel"
+            assert "http://example.com" in context["link"]
 
-@override_settings(EXPORT_JOB_COMPLETION_MAIL_SUBJECT="Django: Export job completed")
-def test_get_export_job_mail_subject():
-    with contextlib.suppress(ImportError):
-        subject = get_export_job_mail_subject()
-        assert subject == "Django: Export job completed"
+    @override_settings(
+        EXPORT_JOB_COMPLETION_MAIL_SUBJECT="Django: Export job completed"
+    )
+    def test_get_export_job_mail_subject(self):
+        with contextlib.suppress(ImportError):
+            subject = get_export_job_mail_subject()
+            assert subject == "Django: Export job completed"
 
+    @override_settings(EXPORT_JOB_COMPLETION_MAIL_TEMPLATE="export_job_completion.html")
+    def test_get_export_job_mail_template(self):
+        with contextlib.suppress(ImportError):
+            template = get_export_job_mail_template()
+            assert template == "export_job_completion.html"
 
-@override_settings(EXPORT_JOB_COMPLETION_MAIL_TEMPLATE="export_job_completion.html")
-def test_get_export_job_mail_template():
-    with contextlib.suppress(ImportError):
-        template = get_export_job_mail_template()
-        assert template == "export_job_completion.html"
+    @patch("import_export_stomp.utils.send_mail")
+    @patch(
+        "import_export_stomp.utils.build_html_and_text_message",
+        return_value=("HTML Message", "Text Message"),
+    )
+    @patch(
+        "import_export_stomp.utils.get_export_job_mail_context",
+        return_value={"context_key": "context_value"},
+    )
+    @patch(
+        "import_export_stomp.utils.get_export_job_mail_template",
+        return_value="test_template.html",
+    )
+    @patch(
+        "import_export_stomp.utils.get_export_job_mail_subject",
+        return_value="Test Subject",
+    )
+    def test_send_export_job_completion_mail(
+        self,
+        mock_subject,
+        mock_template,
+        mock_context,
+        mock_message,
+        mock_send_mail,
+        export_job_mail,
+    ):
+        send_export_job_completion_mail(export_job_mail)
 
-
-def test_send_export_job_completion_mail(export_job, mocker):
-    with contextlib.suppress(ImportError):
-        mocker.patch("import_export_stomp.utils.send_mail")
-        send_export_job_completion_mail(export_job)
-        mocker.assert_called_once()
+        mock_subject.assert_called_once()
+        mock_template.assert_called_once()
+        mock_context.assert_called_once_with(export_job_mail)
+        mock_message.assert_called_once_with(
+            "test_template.html",
+            {"context_key": "context_value", "export_job": export_job_mail},
+        )
+        mock_send_mail.assert_called_once_with(
+            subject="Test Subject",
+            message="Text Message",
+            html_message="HTML Message",
+            from_email="root@localhost",
+            recipient_list=["test@example.com"],
+        )
