35fdfff2cecfdaad8b6a403edaf55ebfeb5b8b72
Nilton Frederico Teixeira <9078708+niltonfrederico@users.noreply.github.com>
2023-10-01 19:18:01 -0300

    refactor: typed and diminished cognite load on some functions

--- import_export_stomp/utils.py
+++ import_export_stomp/utils.py
@@ -1,5 +1,23 @@
+from importlib import import_module
+from typing import Callable
+from typing import Literal
+from typing import Type
+from typing import Union
+from uuid import uuid4
+
 from django.conf import settings
+from django_stomp.builder import build_publisher
+from django_stomp.services.producer import auto_open_close_connection
+from django_stomp.services.producer import do_inside_transaction
 from import_export.formats.base_formats import DEFAULT_FORMATS
+from import_export.resources import ModelResource
+
+IMPORT_EXPORT_STOMP_PROCESSING_QUEUE = getattr(
+    settings,
+    "IMPORT_EXPORT_STOMP_PROCESSING_QUEUE",
+    "django-import-export-stomp-runner",
+)
+
 
 IMPORT_EXPORT_STOMP_EXCLUDED_FORMATS = getattr(
     settings,
@@ -15,3 +33,25 @@ def get_formats():
         if format.TABLIB_MODULE.split(".")[-1].strip("_")
         not in IMPORT_EXPORT_STOMP_EXCLUDED_FORMATS
     ]
+
+
+def send_job_message_to_queue(
+    action: Union[Literal["import"], Literal["export"]], job_id: int
+) -> None:
+    publisher = build_publisher(f"django-import-export-stomp-{str(uuid4())}")
+
+    with auto_open_close_connection(publisher), do_inside_transaction(publisher):
+        publisher.send(
+            queue=IMPORT_EXPORT_STOMP_PROCESSING_QUEUE,
+            body={"action": action, "job_id": str(job_id)},
+        )
+
+
+def resource_importer(resource: str) -> Callable:
+    def main() -> Type[ModelResource]:
+        module, obj = resource.rsplit(".", 1)
+        imported_module = import_module(module)
+
+        return getattr(imported_module, obj)
+
+    return main
