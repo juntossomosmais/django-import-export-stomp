6d8c53583d3a97653bc622d2888105c1cdd02301
Nilton Frederico Teixeira <9078708+niltonfrederico@users.noreply.github.com>
2023-10-18 20:27:18 -0300

    fix: fixed bigautofield and id fields on apps

--- example/winners/apps.py
+++ example/winners/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class ImportExportStompConfig(AppConfig):
+class WinnersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "import_export_stomp"
-    verbose_name = _("Import Export Stomp")
+    name = "winners"
